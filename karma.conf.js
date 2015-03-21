fcac023edbabfdd36a615586a17008d8b40e6149
Matt Travi <programmer@travi.org>
2015-03-21 16:44:05 -0500

    moved the tests for the previewPane module

--- karma.conf.js
+++ karma.conf.js
@@ -12,25 +12,17 @@ module.exports = function (config) {
 
             'bower_components/jquery/dist/jquery.js',
             'bower_components/jsrender/jsrender.js',
-            //'doc_root/resources/thirdparty/moment/moment.js',
-            //'doc_root/resources/thirdparty/jquery-equalizebottoms/jquery.ba-equalizebottoms.js',
-            //'doc_root/resources/thirdparty/travi-slide-nav/jquery.slideNav.js',
             'test/stubs/sublimevideo.js',
             'test/stubs/ReflectionStub.js',
 
             'bower_components/travi-test-utils/stubs/modernizrStub.js',
-            //'doc_root/resources/thirdparty/travi-test-utils/stubs/amplifyStub.js',
             'thirdparty/lightbox/jquery.lightbox.js',
 
             'bower_components/travi-core/js/travi.js',
             'bower_components/travi-core/js/travi/templates.js',
             'bower_components/travi-core/js/travi/events.js',
-            //'doc_root/resources/thirdparty/travi-core/js/travi/dependencies/*.js',
-            //'doc_root/resources/thirdparty/travi-ui/js/pagination.js',
             'test/resources/load-templates.js',
 
-            //'doc_root/resources/js/travi/org.js',
-            //'doc_root/resources/js/travi/org/**/*.js',
             'js/video/*.js',
             'js/photos/previewPane.js',
             'js/photos/*.js',
