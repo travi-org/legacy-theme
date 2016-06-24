d5ea7346b89a58087818cb0c199b9ba25526f8a1
Matt Travi <programmer@travi.org>
2016-06-23 22:41:26 -0500

    updated node version to build against

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,5 +2,11 @@ module.exports = function (grunt) {
     'use strict';
 
     require('time-grunt')(grunt);
-    require('load-grunt-config')(grunt);
+    require('load-grunt-config')(grunt, {
+        jitGrunt: {
+            staticMappings: {
+                scsslint: 'grunt-scss-lint'
+            }
+        }
+    });
 };
